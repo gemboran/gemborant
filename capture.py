786ede09ac00dae97ad20eab6ad8d12a3efc099c
haqi <haqi@buatan.id>
2024-05-31 14:34:30 +0000

    Add FPS Limit

--- capture.py
+++ capture.py
@@ -20,6 +20,7 @@ class Capture:
             with self.lock:
                 self.capture_screen()
             self.update_fps()
+            time.sleep(1/60)
 
     def capture_screen(self):
         with mss() as sct:
